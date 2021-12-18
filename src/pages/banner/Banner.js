d3abe06bde315a984ec3ccb2d218a1f0cb229243
sheikh ariful <sheikharifulislam770@gmail.com>
2021-12-18 22:52:25 +0600

    update banner

--- src/pages/banner/Banner.js
+++ src/pages/banner/Banner.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom';
 import './banner.css';
 import bannerImage from  '../../images/banner.webp'
 const Banner = () => {
-
-    const navigate = useNavigate()
-
-    const handleMoreProductBtn = () => {
-        navigate('/all-products',{
-            replace: true,
-        })
-    }
-
     return (
         <div className="web-banner">
             <picture className="banner-image">
